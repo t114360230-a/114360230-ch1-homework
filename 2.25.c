f693dcdfb54ec42c6f56b3de09b0453d167bdcf8
aimee811 <aimee.chen.070811@gmail.com>
2025-10-22 13:27:51 +0800

    2.25 revise

--- 2.25.c
+++ 2.25.c
@@ -2,24 +2,19 @@
 #include <stdlib.h>
 
 void print_big_C() {
-    printf("  CCCCC\n");
-    printf(" C\n");
-    printf("C\n");
-    printf("C\n");
-    printf("C\n");
-    printf(" C\n");
-    printf("  CCCCC\n");
+    printf(" CCCCCCCCCCCCC \n");
+    printf("C             C\n");
+    printf("C             C\n");
+    printf("C             C\n");
 }
 
-// 印出大寫的 H
+
 void print_big_H() {
-    printf("H     H\n");
-    printf("H     H\n");
-    printf("H     H\n");
-    printf("HHHHHHH\n");
-    printf("H     H\n");
-    printf("H     H\n");
-    printf("H     H\n");
+    printf("HHHHHHHHHHHHHHH\n");
+    printf("       H       \n");
+    printf("       H       \n");
+    printf("       H       \n");
+    printf("HHHHHHHHHHHHHHH\n");
 }
 
 int main() {
