003e8bda0a7cb282e2facbeeafd65188e4fc23ab
aimee811 <aimee.chen.070811@gmail.com>
2025-10-23 01:51:55 +0800

    final

--- 2.27.c
+++ 2.27.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-    int i;
-    for(i=0;i<4;i++){
-        printf("* * * * * * * * \n * * * * * * * * \n");
+    int i, j;
+    for(i = 1; i <= 5; i++) {
+        for(j = 5; j > i; j--) printf(" ");
+        for(j = 1; j <= 2*i-1; j++) printf("*");
+        printf("\n");
     }
         
     };
